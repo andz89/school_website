0249c31b40e51ea1989abb9febb3d33f13e35fc3
andz <andzrivero89@gmail.com>
2021-02-06 23:48:02 -0800

    checked

--- dist/script/script.js
+++ dist/script/script.js
@@ -2,7 +2,7 @@ const scroll = new SmoothScroll('nav a[href*="#"]', {
 	speed: 500
 });
 
-let btn = document.querySelector('#btn');
+let btn = document.querySelector('i');
 let sideMenu = document.querySelector(".nav-container-bar");
 let btnclose = document.querySelector(".btn-close")
 
@@ -19,12 +19,12 @@ btn.addEventListener("click", ()=>{
 btnclose.addEventListener("click", ()=>{
 	console.log("hello")
 	sideMenu.style.marginLeft = "-1300px";
-	
+	// btn.classList.toggle('active')
 });
 
 
 sideMenu.addEventListener('click', ()=>{
 	btn.classList.toggle('active');
-	sideMenu.classList.toggle('active');
+	sideMenu.style.marginLeft="-1300px";
 	
 })
