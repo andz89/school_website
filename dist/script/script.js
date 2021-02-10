6a9e68c75d249eec81ef626ef3d3a8897b60709d
andz <andzrivero89@gmail.com>
2021-02-10 07:56:08 -0800

    add time

--- dist/script/script.js
+++ dist/script/script.js
@@ -8,29 +8,17 @@ let btnclose = document.querySelector(".btn-close")
 let li = document.querySelectorAll('li')
 
 btn.addEventListener("click", ()=>{
-	// sideMenu.classList.toggle('nav-active');
-	
 	sideMenu.style.transform = "translateX(0%)";
 });
-
-
 
 btnclose.addEventListener("click", ()=>{
 	sideMenu.style.transform = "translateX(100%)";
 	
 });
 
-// sideMenu.addEventListener("click", ()=>{
-// 	li.forEach((list)=>{
-
-// 	})
-	
-// });
-
-
-// setTimeout(function () {
-//     location.reload();
-// }, 120000);
+setTimeout(function () {
+    location.reload();
+}, 900000);
 
 
 
