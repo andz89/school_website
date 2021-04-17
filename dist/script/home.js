ce71a6fd1a2163770dd650f50d2459624743a002
andz <andzrivero89@gmail.com>
2021-04-16 20:39:45 -0700

    add img

--- dist/script/home.js
+++ dist/script/home.js
@@ -6,14 +6,14 @@ setTimeout(function () {
 
 
 //about bvmces- to show hidden paragraph
-let btnAbout = document.querySelector('#read-btn-about')
-let aboutExpand = document.querySelector("#about-div")
-let hidePara = document.querySelector(".hide-about-paragraph")
-btnAbout.addEventListener('click', ()=>{
-	aboutExpand.classList.toggle('about-active')
-	hidePara.classList.toggle('show')
-	btnAbout.innerHTML ="Show less"
-})
+// let btnAbout = document.querySelector('#read-btn-about')
+// let aboutExpand = document.querySelector("#about-div")
+// let hidePara = document.querySelector(".hide-about-paragraph")
+// btnAbout.addEventListener('click', ()=>{
+// 	aboutExpand.classList.toggle('about-active')
+// 	hidePara.classList.toggle('show')
+// 	btnAbout.innerHTML ="Show less"
+// })
 
 
 
